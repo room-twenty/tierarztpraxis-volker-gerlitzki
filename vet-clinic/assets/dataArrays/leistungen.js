19ae83a66a745a5efb881e74a0988131fd6b2dcb
mrbubbles-src <manuel_fahrenholz@web.de>
2024-07-31 21:49:25 +0200

    feat: refactor leistungen.js to use array of items
    The commit refactors the `leistungen.js` file in the `vet-clinic` project to use an array of `items` instead of individual properties for each item. This change improves the structure and readability of the code.

--- vet-clinic/assets/dataArrays/leistungen.js
+++ vet-clinic/assets/dataArrays/leistungen.js
@@ -1,45 +1,49 @@
 export const leistungen = [
-    {
-        title: "Diagnostik",
-        itemOne: "Röntgendiagnostik",
-        itemTwo: "Ultraschall-Untersuchungen",
-        itemThree: "Laboruntersuchungen",
-        itemFour: "EKG",
-        itemFive: "Allergietests und Desensibilisierung",
-        itemSix: "Geriatrie-Check",
-    },
-    {
-        title: "Physiotherapeutische Anwendungen",
-        itemOne: "nach orthopädischen Operationen (z.B. zur Schmerztherapie)",
-        itemTwo: "Revitalisierung im Bewegungsapparat (Muskelaufbau)",
-        itemThree:
-            "zur Motivation der Bewegung bei älteren Tieren oder zur Gewichtsreduktion",
-    },
-    {
-        title: "Weitere Leistungen",
-        itemOne: "Augenheilkunde",
-        itemTwo: "Zahnheilkunde",
-        itemThree: "Elektronische Tierkennzeichnung/EU Heimtierpässe",
-        itemFour: "Tierärztliche Vollapotheke",
-        itemFive: "Große Auswahl an Diätfuttermitteln",
-        itemSix: "Impferinnerung",
-        itemSeven: "EC-Kartenzahlung",
-        itemEight: "Hunde- und Katzenfriseur",
-    },
-    {
-        title: "Züchter Spezifische Analysen",
-        itemOne: "DNA-Bestimmungen",
-        itemTwo: "Blutgruppen-Bestimmung",
-        itemThree: "Reihenuntersuchen",
-    },
-    {
-        title: "Beratung",
-        itemOne: "Beratung bei Verhaltensproblemen",
-        itemTwo: "Ernährungsberatung",
-    },
-    {
-        title: "Operationen und Chirurgie",
-        itemOne: "Injektions-, Inhalations- und Kurznarkosen",
-        itemTwo: "Weichteil- und Knochenchirurgie",
-    },
+  {
+    title: 'Diagnostik',
+    items: [
+      'Röntgendiagnostik',
+      'Ultraschall-Untersuchungen',
+      'Laboruntersuchungen',
+      'EKG',
+      'Allergietests und Desensibilisierung',
+      'Geriatrie-Check',
+    ],
+  },
+  {
+    title: 'Physiotherapeutische Anwendungen',
+    items: [
+      'Nach orthopädischen Operationen (z.B. zur Schmerztherapie)',
+      'Revitalisierung im Bewegungsapparat (Muskelaufbau)',
+      'Zur Motivation der Bewegung bei älteren Tieren oder zur Gewichtsreduktion',
+    ],
+  },
+  {
+    title: 'Weitere Leistungen',
+    items: [
+      'Augenheilkunde',
+      'Zahnheilkunde',
+      'Elektronische Tierkennzeichnung/EU Heimtierpässe',
+      'Tierärztliche Vollapotheke',
+      'Große Auswahl an Diätfuttermitteln',
+      'Impferinnerung',
+      'EC-Kartenzahlung',
+      'Hunde- und Katzenfriseur',
+    ],
+  },
+  {
+    title: 'Züchter Spezifische Analysen',
+    items: ['DNA-Bestimmungen', 'Blutgruppen-Bestimmung', 'Reihenuntersuchen'],
+  },
+  {
+    title: 'Beratung',
+    items: ['Beratung bei Verhaltensproblemen', 'Ernährungsberatung'],
+  },
+  {
+    title: 'Operationen und Chirurgie',
+    items: [
+      'Injektions-, Inhalations- und Kurznarkosen',
+      'Weichteil- und Knochenchirurgie',
+    ],
+  },
 ];
