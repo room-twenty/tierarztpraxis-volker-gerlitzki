42e518cfae226d81dec19b21869b2e39a09d0ffa
mrbubbles-src <manuel_fahrenholz@web.de>
2024-07-31 21:01:24 +0200

    feat: add Leistungen component

--- vet-clinic/app/page.tsx
+++ vet-clinic/app/page.tsx
@@ -1,7 +1,10 @@
+import Leistungen from '@/components/Leistungen/Leistungen';
+
 export default function Home() {
-    return (
-        <>
-            <h1>Ich bin die Startseite</h1>
-        </>
-    );
+  return (
+    <>
+      {/* <h1>Ich bin die Startseite</h1> */}
+      <Leistungen />
+    </>
+  );
 }
