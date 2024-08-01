946d1af49a4ffd8081dcb387def49d2ed02cb513
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-01 19:52:56 +0200

    feat: Add PraxisTeam component and update Home page
    The commit adds the PraxisTeam component to the Home page of the vet-clinic project. This component is responsible for rendering the team members on the website. It imports the necessary dependencies and displays the team members using the TeamCard component. This change enhances the website by showcasing the team members and their information.

--- vet-clinic/app/page.tsx
+++ vet-clinic/app/page.tsx
@@ -1,10 +1,12 @@
 import Leistungen from '@/components/Leistungen/Leistungen';
+import PraxisTeam from '@/components/PraxisTeam/PraxisTeam';
 
 export default function Home() {
   return (
     <>
       {/* <h1>Ich bin die Startseite</h1> */}
       <Leistungen />
+      <PraxisTeam />
     </>
   );
 }
