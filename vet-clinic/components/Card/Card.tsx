c2889c4f268edb7f368a889ee8b64e3cb471fbdd
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-01 18:17:37 +0200

    refactor: Update Card component to use FontAwesome icons and improve styling
    The commit updates the Card component in the Card.tsx file to include FontAwesome icons and improve the styling. It imports the necessary dependencies from @fortawesome/free-solid-svg-icons and @fortawesome/react-fontawesome. The component now displays a FontAwesome icon for each item in the items array, and the overall styling has been enhanced with rounded corners, shadows, and consistent spacing. This change improves the visual appeal and user experience of the Card component.

--- vet-clinic/components/Card/Card.tsx
+++ vet-clinic/components/Card/Card.tsx
@@ -1,5 +1,35 @@
-const Card = () => {
-  return <div></div>;
+import { ILeistungen } from '@/assets/dataArrays/leistungen';
+import { faPaw } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const Card = ({ title, items, faIcon }: ILeistungen) => {
+  return (
+    <div
+      className="m-4 flex flex-col rounded-lg p-4 shadow-md shadow-lblue"
+      key={title}>
+      <FontAwesomeIcon
+        icon={faIcon}
+        className="w-12 place-self-center text-dblue"
+      />
+      <h2 className="my-2 text-pretty text-center text-[1.3rem] font-semibold text-dblue underline decoration-lblue underline-offset-[7px]">
+        {title}
+      </h2>
+      <ul className="fa-ul mt-">
+        {items.map((item, index) => (
+          <li key={item + index} className="flex flex-row text-pretty">
+            <span className="fa-li">
+              {
+                <FontAwesomeIcon
+                  className="mr-1 mt-[0.2rem] w-[1rem] text-lblue"
+                  icon={faPaw}
+                />
+              }
+            </span>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Card;
