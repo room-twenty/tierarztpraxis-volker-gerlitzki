579d1579039c8a5842a06cf3f1f822da0556523d
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-02 22:16:47 +0200

    chore: rename file

--- vet-clinic/components/DocTeamSection/DocTeamSection.tsx
+++ vet-clinic/components/DocTeamSection/DocTeamSection.tsx
@@ -1,15 +1,15 @@
-import { ITeam, team } from '@/assets/dataArrays/team';
+import { ITeam, team } from '@/assets/dataArrays/aerzte';
 import { TeamCard } from '../Card/Card';
 
-const PraxisTeam = () => {
+const DocTeamSection = () => {
   return (
-    <>
-      <h2>Unser Team</h2>
+    <section>
+      <h2>Unsere Ärzte</h2>
       {team.map((mitglied: ITeam, index) => (
         <TeamCard key={mitglied.name + index + 1} {...mitglied} />
       ))}
-    </>
+    </section>
   );
 };
 
-export default PraxisTeam;
+export default DocTeamSection;
