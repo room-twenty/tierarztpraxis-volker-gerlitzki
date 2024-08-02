4dd2b39785b248d413fb0255b58be0c40094ad47
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-02 23:20:48 +0200

    feat: improve component to show link only when not on leistungen route

--- vet-clinic/components/LeistungenSection/LeistungenSection.tsx
+++ vet-clinic/components/LeistungenSection/LeistungenSection.tsx
@@ -1,19 +1,22 @@
 import { ILeistungen, leistungen } from '@/assets/dataArrays/leistungen';
 import { LeistungenCard } from '../Card/Card';
 import Link from 'next/link';
-
 interface LeistungenSectionProps {
   sliceAmount: [number, number?];
+  currentPath: string;
 }
-
-const LeistungenSection = ({ sliceAmount }: LeistungenSectionProps) => {
+const LeistungenSection = ({
+  sliceAmount,
+  currentPath,
+}: LeistungenSectionProps) => {
+  const isPathLeistungen = currentPath === '/leistungen';
   return (
     <section>
       <h2>Unsere Leistungen</h2>
       {leistungen.slice(...sliceAmount).map((leistung: ILeistungen, index) => (
         <LeistungenCard key={leistung.title + index + 1} {...leistung} />
       ))}
-      <Link href={'/leistungen'}>mehr leistungen</Link>
+      {!isPathLeistungen && <Link href={'/leistungen'}>mehr leistungen</Link>}
     </section>
   );
 };
