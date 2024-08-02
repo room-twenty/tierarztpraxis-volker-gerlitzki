7b6d2788b7283e69e3a262f0bba083dbf825ec03
jacqueline-s-w <jacqueline.scharrer@web.de>
2024-08-02 17:24:35 +0200

    style: getestet

--- vet-clinic/components/Header/TopHeader.tsx
+++ vet-clinic/components/Header/TopHeader.tsx
@@ -5,25 +5,35 @@ import Image from 'next/image';
 
 const TopHeader = () => {
   return (
-    <header className="flex items-center bg-blue-200">
-      <a href="tel:+49407214244">
-        <span className="hover: hover:text-red-800">
-          <FontAwesomeIcon className="w-[1rem]" icon={faPhone} />
-          Anruf
-        </span>
-      </a>
+    <header className="border-md items-space-around flex justify-evenly border-b border-blue-900 bg-white">
+      <button className="h-auto w-10 rounded-md border-2 border-solid border-blue-900 bg-green-400">
+        <a href="tel:+49407214244">
+          <span>
+            <FontAwesomeIcon
+              className="h-auto w-[2rem] text-white"
+              icon={faPhone}
+            />
+            {/*Anruf*/}
+          </span>
+        </a>
+      </button>
 
       <Image
         className="w-[15rem] text-blue-900"
         src={Logo}
         alt="Dr. Gerlitki Logo"
       />
-      <a href="#">
-        <span className="hover: hover:text-red-800">
-          <FontAwesomeIcon className="w-[1rem]" icon={faLocationDot} />
-          Anfahrt
-        </span>
-      </a>
+      <button className="h-auto w-10 rounded-md border-2 border-solid border-black bg-blue-300">
+        <a href="#">
+          <span>
+            <FontAwesomeIcon
+              className="w-[2rem] text-white"
+              icon={faLocationDot}
+            />
+            {/*Anfahrt*/}
+          </span>
+        </a>
+      </button>
     </header>
   );
 };
