b83378fe472ba5657fc47629e611efe915ba26c6
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-02 22:17:35 +0200

    chore: remove commented code

--- vet-clinic/components/Card/Card.tsx
+++ vet-clinic/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import { ILeistungen } from '@/assets/dataArrays/leistungen';
-import { ITeam } from '@/assets/dataArrays/team';
+import { ITeam } from '@/assets/dataArrays/aerzte';
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
@@ -55,68 +55,3 @@ const TeamCard = ({ imgSrc, name, infoTexte }: ITeam) => (
 );
 
 export { LeistungenCard, TeamCard };
-
-// Alternative die Copilot vorgeschlagen hat. Verstehe ich aber nur bedingt und finde ich zu kompliziert
-// Wird bei zeiten gelöscht!
-
-// import { ILeistungen } from '@/assets/dataArrays/leistungen';
-// import { ITeam } from '@/assets/dataArrays/team';
-// import { faPaw } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import Image from 'next/image';
-
-// const Card = (props: ILeistungen | ITeam) => {
-//   const isLeistungen = (props: ILeistungen | ITeam): props is ILeistungen => {
-//     return (props as ILeistungen).title !== undefined;
-//   };
-
-//   return (
-//     <div
-//       className="m-4 flex flex-col rounded-lg p-4 shadow-md shadow-lblue"
-//       key={isLeistungen(props) ? props.title : props.name}>
-//       {isLeistungen(props) && props.faIcon && (
-//         <FontAwesomeIcon
-//           icon={props.faIcon}
-//           className="w-12 place-self-center text-dblue"
-//         />
-//       )}
-//       {'imgSrc' in props && props.imgSrc && (
-//         <Image
-//           src={props.imgSrc}
-//           alt={props.name || ''}
-//           fill
-//           className="w-[20rem]"
-//         />
-//       )}
-//       {isLeistungen(props) && props.title && (
-//         <h2 className="my-2 text-pretty text-center text-[1.3rem] font-semibold text-dblue underline decoration-lblue underline-offset-[7px]">
-//           {props.title}
-//         </h2>
-//       )}
-//       {'name' in props && props.name && (
-//         <h2 className="my-2 text-pretty text-center text-[1.3rem] font-semibold text-dblue underline decoration-lblue underline-offset-[7px]">
-//           {props.name}
-//         </h2>
-//       )}
-//       {isLeistungen(props) && props.items && (
-//         <ul className="fa-ul mt-">
-//           {props.items.map((item, index) => (
-//             <li key={item + index} className="flex flex-row text-pretty">
-//               <span className="fa-li">
-//                 <FontAwesomeIcon
-//                   className="mr-1 mt-[0.2rem] w-[1rem] text-lblue"
-//                   icon={faPaw}
-//                 />
-//               </span>
-//               {item}
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//       {'infoTexte' in props &&
-//         props.infoTexte.map((infoText, index) => <p key={index}>{infoText}</p>)}
-//     </div>
-//   );
-// };
-
-// export default Card;
