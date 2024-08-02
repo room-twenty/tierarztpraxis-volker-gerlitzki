be38556f91bba48f567f051b56e1a852f0ad0c74
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-02 22:44:06 +0200

    feat: improve slice method to take unspecific values and make it typesafe

--- vet-clinic/components/LeistungenSection/LeistungenSection.tsx
+++ vet-clinic/components/LeistungenSection/LeistungenSection.tsx
@@ -2,11 +2,15 @@ import { ILeistungen, leistungen } from '@/assets/dataArrays/leistungen';
 import { LeistungenCard } from '../Card/Card';
 import Link from 'next/link';
 
-const LeistungenSection = () => {
+interface LeistungenSectionProps {
+  sliceAmount: [number, number?];
+}
+
+const LeistungenSection = ({ sliceAmount }: LeistungenSectionProps) => {
   return (
     <section>
       <h2>Unsere Leistungen</h2>
-      {leistungen.slice(0, 3).map((leistung: ILeistungen, index) => (
+      {leistungen.slice(...sliceAmount).map((leistung: ILeistungen, index) => (
         <LeistungenCard key={leistung.title + index + 1} {...leistung} />
       ))}
       <Link href={'/leistungen'}>mehr leistungen</Link>
