89de7373ed3037b33beeef19f2a2e55debeaa7aa
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-02 22:18:22 +0200

    chore: rename file to better reflect its purpose. add slice to reduce amount shown on home

--- vet-clinic/components/LeistungenSection/LeistungenSection.tsx
+++ vet-clinic/components/LeistungenSection/LeistungenSection.tsx
@@ -1,15 +1,17 @@
 import { ILeistungen, leistungen } from '@/assets/dataArrays/leistungen';
 import { LeistungenCard } from '../Card/Card';
+import Link from 'next/link';
 
-const Leistungen = () => {
+const LeistungenSection = () => {
   return (
-    <>
+    <section>
       <h2>Unsere Leistungen</h2>
-      {leistungen.map((leistung: ILeistungen, index) => (
+      {leistungen.slice(0, 3).map((leistung: ILeistungen, index) => (
         <LeistungenCard key={leistung.title + index + 1} {...leistung} />
       ))}
-    </>
+      <Link href={'/leistungen'}>mehr leistungen</Link>
+    </section>
   );
 };
 
-export default Leistungen;
+export default LeistungenSection;
