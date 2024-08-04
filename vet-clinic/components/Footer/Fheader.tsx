4bac1ed30b8749393e353a71b1de24a45bad4603
metinarsu <mmarsu@outlook.de>
2024-08-04 14:42:06 +0200

    feat: added base styling to build components

--- vet-clinic/components/Footer/Fheader.tsx
+++ vet-clinic/components/Footer/Fheader.tsx
@@ -1,10 +1,10 @@
 const Fheader = () => {
   return (
-    <>
+    <div className="p-4 text-center">
       {/* Just an heading with empty div for shadows / decoration, animation: fade in from bot*/}
       <h2>Haben sie eine Frage? Brauchen Sie einen Termin?</h2>
-      <div>-----------</div>
-    </>
+      <div className="h-[1px] bg-white"></div>
+    </div>
   );
 };
 
---
+++ vet-clinic/components/Footer/Fmap.tsx
@@ -0,0 +1,15 @@
+const Fmap = () => {
+  return (
+    <>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2373.943195060647!2d10.21594511342275!3d53.48734477221505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b1f26202feaddd%3A0xc453e92bc08f5d39!2sDr.%20Volker%20Robert%20Gerlitzki!5e0!3m2!1sde!2sde!4v1722773235454!5m2!1sde!2sde"
+        width="500"
+        height="450"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      />
+    </>
+  );
+};
+
+export default Fmap;
