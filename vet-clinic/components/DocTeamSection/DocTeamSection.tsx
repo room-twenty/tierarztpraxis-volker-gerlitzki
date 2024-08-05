8f775d543a6223c8cc863e5185b231cd345c4008
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-05 22:11:55 +0200

    refactor: update DocTeamSection to include id attribute for better accessibility

--- vet-clinic/components/DocTeamSection/DocTeamSection.tsx
+++ vet-clinic/components/DocTeamSection/DocTeamSection.tsx
@@ -3,8 +3,8 @@ import { TeamCard } from '../Card/Card';
 
 const DocTeamSection = () => {
   return (
-    <section>
-      <h2 className="text-dblue text-center text-[1.7rem] font-semibold">
+    <section id="unsere-aerzte">
+      <h2 className="text-center text-[1.7rem] font-semibold text-dblue">
         Unsere Ärzte
       </h2>
       {team.map((mitglied: ITeam, index) => (
