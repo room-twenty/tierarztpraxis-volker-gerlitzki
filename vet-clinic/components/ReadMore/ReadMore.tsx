53968bef658a429049a70656e1d482f64853667a
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-05 21:40:22 +0200

    feat: create ReadMore Component to reduce or expand text shown in cards

---
+++ vet-clinic/components/ReadMore/ReadMore.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React, { useState } from 'react';
+
+const ReadMore = ({
+  amountShown,
+  children,
+}: {
+  amountShown: number;
+  children: React.ReactNode;
+}) => {
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+
+  const childrenArray = React.Children.toArray(children);
+  const childrenToShow = isReadMore
+    ? childrenArray.slice(0, amountShown)
+    : childrenArray;
+
+  return (
+    <>
+      {childrenToShow}
+      {childrenArray.length > amountShown && (
+        <span onClick={toggleReadMore} className="cursor-pointer text-dblue">
+          <strong>{isReadMore ? '...mehr lesen' : '...weniger lesen'}</strong>
+        </span>
+      )}
+    </>
+  );
+};
+
+export default ReadMore;
