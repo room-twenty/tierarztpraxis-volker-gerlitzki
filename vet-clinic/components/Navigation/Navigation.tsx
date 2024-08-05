0d540fa43062ab1eeec591405f43332a19146646
jacqueline-s-w <jacqueline.scharrer@web.de>
2024-08-05 14:11:03 +0200

    style: positioning, bg-color,font-color

--- vet-clinic/components/Navigation/Navigation.tsx
+++ vet-clinic/components/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Navigation = () => {
   return (
-    <div className="no-wrap flex list-none">
+    <div className="no-wrap montserrat flex list-none justify-evenly bg-blue-300 text-white">
       <li>
         <a href="./">Startseite</a>
       </li>
