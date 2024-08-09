92b4bfbe36d592e204daa621bfcb3e71824234bc
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-09 21:25:14 +0200

    chore: Refactor TopHeader component for improved layout and accessibility

--- vet-clinic/components/Header/TopHeader.tsx
+++ vet-clinic/components/Header/TopHeader.tsx
@@ -5,23 +5,25 @@ import Image from 'next/image';
 
 const TopHeader = () => {
   return (
-    <header className="border-md items-space-around bg-white flex flex-col place-items-center p-2">
-      <div className="flex w-[100%] flex-row justify-between gap-5">
-        <button className="bg-green-400 text-white h-12 w-32 rounded-md border-2 p-2">
+    <header className="flex flex-row place-items-center bg-white p-2">
+      <div className="">
+        <Image src={Logo} alt="Dr. Gerlitki Logo" />
+      </div>
+      <div className="">
+        <button className="h-12 w-32 rounded-md border-2 bg-green-400 p-2 text-white">
           <a href="tel:+49407214244" className="flex justify-center gap-2">
             <FontAwesomeIcon className="w-4" icon={faPhone} />
             Anruf
           </a>
         </button>
 
-        <button className="text-white h-12 w-32 rounded-md border-2 bg-lblue p-2">
+        <button className="h-12 w-32 rounded-md border-2 bg-lblue p-2 text-white">
           <a href="#" className="flex justify-center gap-2">
             <FontAwesomeIcon className="w-4" icon={faLocationDot} />
             Anfahrt
           </a>
         </button>
       </div>
-      <Image className="" src={Logo} alt="Dr. Gerlitki Logo" />
     </header>
   );
 };
