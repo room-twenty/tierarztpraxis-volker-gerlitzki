ab709b5ae1b47f727077a3a3f096a84cf32bc8a7
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-11 20:33:15 +0200

    chore: Improve styling and animation duration of NavLink component

--- vet-clinic/components/NavLink/NavLink.tsx
+++ vet-clinic/components/NavLink/NavLink.tsx
@@ -14,7 +14,7 @@ const NavLink = ({ href, children }: NavLinkProps) => {
       href={href}
       className={
         isActive(href)
-          ? 'under scale-110 font-semibold text-steel-blue-50 underline underline-offset-[4px] duration-500 ease-in-out'
+          ? 'scale-110 font-semibold text-steel-blue-50 underline underline-offset-[4px] duration-500 ease-in-out'
           : 'font-normal text-steel-blue-50 duration-500 ease-in-out hover:scale-110 hover:font-semibold hover:underline hover:underline-offset-[4px]'
       }>
       {children}
