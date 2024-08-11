c53b1f09c35024638fd1b86375784951eac50b81
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-11 18:30:27 +0200

    refactor: Update Footer component to include an ID for anchor link to 'anfahrt'

--- vet-clinic/components/Footer/Footer.tsx
+++ vet-clinic/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Fmap from './Fmap';
 
 const Footer = () => {
   return (
-    <section className="bg-lblue p-2 pb-10 text-white">
+    <section id="anfahrt" className="bg-lblue p-2 pb-10 text-white">
       {/* use framer motion here for master animation -> roll up from the bot */}
       <Fheader />
       <Fcontact />
