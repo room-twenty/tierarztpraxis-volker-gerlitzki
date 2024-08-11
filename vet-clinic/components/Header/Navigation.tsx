dc3549c5b485e5a92de745d7648189b35ce3515c
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-11 19:21:21 +0200

    chore: Update Navigation component to fix link to 'Leistungen' and improve styling

--- vet-clinic/components/Header/Navigation.tsx
+++ vet-clinic/components/Header/Navigation.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import NavLink from '@/components/NavLink/NavLink';
 
 const Navigation = () => {
   return (
     <nav className="flex h-12 place-items-center justify-evenly bg-glacier-300">
       <NavLink href="/">Startseite</NavLink>
-      <NavLink href="/leistungen">Leistungen</NavLink>
+      <NavLink href="/leistungen">Alle Leistungen</NavLink>
       <NavLink href="/#unsere-aerzte">Unsere Ärzte</NavLink>
       <NavLink href="/blog">Blog</NavLink>
     </nav>
