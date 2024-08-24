c81553aeedc809d6245ae74350cbaffec0d40094
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-24 19:37:02 +0200

    chore: Update navigation link order

--- vet-clinic/components/Header/Navigation.tsx
+++ vet-clinic/components/Header/Navigation.tsx
@@ -4,8 +4,8 @@ const Navigation = () => {
   return (
     <nav className="flex h-12 place-items-center justify-evenly bg-glacier-300">
       <NavLink href="/">Startseite</NavLink>
-      <NavLink href="/aerzte">Unsere Ärzte</NavLink>
       <NavLink href="/tiere">Tiere</NavLink>
+      <NavLink href="/aerzte">Unsere Ärzte</NavLink>
       <NavLink href="/leistungen">Alle Leistungen</NavLink>
       <NavLink href="/blog">Blog</NavLink>
     </nav>
