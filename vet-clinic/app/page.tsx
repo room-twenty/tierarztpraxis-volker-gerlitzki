99859625e4bfd7782418bc0c2604d8e7de97dd11
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-24 19:30:11 +0200

    feat: Add ClinicPhilosophy component to homepage

--- vet-clinic/app/page.tsx
+++ vet-clinic/app/page.tsx
@@ -1,11 +1,13 @@
 import LeistungenSection from '@/components/LeistungenSection/LeistungenSection';
 import DocTeamSection from '@/components/DocTeamSection/DocTeamSection';
 import HeroSection from '@/components/HeroSection/HeroSection';
+import ClinicPhilosophy from '@/components/ClinicPhilosophy/ClinicPhilosophy';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <ClinicPhilosophy />
       {/* <LeistungenSection
         sliceAmount={[0, 3]}
         currentPath="/"
