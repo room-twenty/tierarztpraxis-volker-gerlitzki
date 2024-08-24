88f34f30c88acc108aebdaf718f0113de916a6e1
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-24 19:36:12 +0200

    feat: Update navigation link for tiere page

--- vet-clinic/components/Header/Navigation.tsx
+++ vet-clinic/components/Header/Navigation.tsx
@@ -4,8 +4,9 @@ const Navigation = () => {
   return (
     <nav className="flex h-12 place-items-center justify-evenly bg-glacier-300">
       <NavLink href="/">Startseite</NavLink>
-      <NavLink href="/leistungen">Alle Leistungen</NavLink>
       <NavLink href="/aerzte">Unsere Ärzte</NavLink>
+      <NavLink href="/tiere">Tiere</NavLink>
+      <NavLink href="/leistungen">Alle Leistungen</NavLink>
       <NavLink href="/blog">Blog</NavLink>
     </nav>
   );
