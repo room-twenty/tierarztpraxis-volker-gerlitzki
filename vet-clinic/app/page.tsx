d139e1c90894d525878d12009f9f6e3b808189a5
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-24 19:08:52 +0200

    chore: Remove commented out code and unused component

--- vet-clinic/app/page.tsx
+++ vet-clinic/app/page.tsx
@@ -5,14 +5,13 @@ import HeroSection from '@/components/HeroSection/HeroSection';
 export default function Home() {
   return (
     <>
-      {/* <h1>Ich bin die Startseite</h1> */}
       <HeroSection />
       {/* <LeistungenSection
         sliceAmount={[0, 3]}
         currentPath="/"
         styling="flex flex-col"
-      />
-      <DocTeamSection /> */}
+      /> */}
+      {/* <DocTeamSection /> */}
     </>
   );
 }
