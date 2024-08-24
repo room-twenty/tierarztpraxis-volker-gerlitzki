01b6d3f35826bb27136adfbff4932e32522ceeb9
mrbubbles-src <manuel_fahrenholz@web.de>
2024-08-24 19:35:18 +0200

    chore: Add TierInfoPage and TiereMainPage components for vet clinic pages

---
+++ vet-clinic/app/tiere/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const TierInfoPage = () => {
+  return <div></div>;
+};
+
+export default TierInfoPage;
---
+++ vet-clinic/app/tiere/page.tsx
@@ -0,0 +1,5 @@
+const TiereMainPage = () => {
+  return <div></div>;
+};
+
+export default TiereMainPage;
