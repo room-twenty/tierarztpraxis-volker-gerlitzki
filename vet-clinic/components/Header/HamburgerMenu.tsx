a43f30ceae53830edb3a0d6063aca4bf30178bfb
MetinArsu <mmarsu@outlook.de>
2024-09-18 11:03:12 +0200

    feat: add drawer to hamburger menu

--- vet-clinic/components/Header/HamburgerMenu.tsx
+++ vet-clinic/components/Header/HamburgerMenu.tsx
@@ -4,7 +4,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const HamburgerMenu = () => {
   return (
-    <FontAwesomeIcon className="p-6 text-3xl text-white" icon={faBars} />
+    <div className="drawer w-auto">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <label htmlFor="my-drawer" className="">
+        <FontAwesomeIcon className="p-6 text-3xl text-white" icon={faBars} />
+      </label>
+      <div className="drawer-side z-50">
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"></label>
+        <ul className="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
