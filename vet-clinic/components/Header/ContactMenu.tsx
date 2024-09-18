2d36799cff0756f633981ffc0e4f531eab66c64d
MetinArsu <mmarsu@outlook.de>
2024-09-18 11:08:17 +0200

    feat: add drawer to contact menu

--- vet-clinic/components/Header/ContactMenu.tsx
+++ vet-clinic/components/Header/ContactMenu.tsx
@@ -3,7 +3,29 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 const ContactMenu = () => {
-  return <FontAwesomeIcon icon={faPhone} className='text-3xl p-6'/>;
+  return (
+    <div className="drawer drawer-end w-auto">
+      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+      <label htmlFor="my-drawer-4">
+        <FontAwesomeIcon icon={faPhone} className="p-6 text-3xl" />
+      </label>
+      <div className="drawer-side z-50">
+        <label
+          htmlFor="my-drawer-4"
+          aria-label="close sidebar"
+          className="drawer-overlay"></label>
+        <ul className="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ContactMenu;
